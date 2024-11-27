180857ddb4cbc93d74a82f9879e112f57d7e72c7
heliguy <heliguy4599@hotmail.com>
2024-11-27 18:15:31 -0500

    Add EntryRow widget

---
+++ src/entry_row.js
@@ -0,0 +1,13 @@
+import GObject from 'gi://GObject';
+import Gtk from 'gi://Gtk';
+import Adw from 'gi://Adw';
+
+export const EntryRow = GObject.registerClass({
+	GTypeName: 'IgnitionWindow',
+	Template: 'resource:///io/github/flattool/Ignition/gtk/entry-row.ui',
+	InternalChildren: ['label'],
+}, class EntryRow extends Adw.ActionRow {
+	constructor(...args) {
+		super(...args);
+	}
+});
